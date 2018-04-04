c5e7e9abae10223675dff5f1584b60e751bb98c0
1amBulletproof <brandon.tarney@gmail.com>
2018-04-04 01:44:52 -0400

    +video player working and asset image previews available - need constraints tho

--- tarney_homework4/tarney_homework4/VideoViewController.swift
+++ tarney_homework4/tarney_homework4/VideoViewController.swift
@@ -8,33 +8,74 @@
 import UIKit
 import AVKit
 import AVFoundation
+import CoreMedia
 
 class VideoViewController: UIViewController, AVPlayerViewControllerDelegate  {
-    //TODO:
-//        1. Create Thumbnails for videos
-//            - Use AVAssetImageGenerator async generation
-//        1. Segue to proper full-screen video player
     
     @IBOutlet weak var thumbNail1: UIImageView!
     @IBOutlet weak var thumbNail2: UIImageView!
     @IBOutlet weak var thumbNail3: UIImageView!
     
+    let video1Url = "https://www.cdc.gov/flu/video/flushots_60sec.mp4"
+    let video2Url = "https://www.cdc.gov/flu/video/who-needs-flu-vaccine-30_320px.mp4"
+    let video3Url = "https://www.cdc.gov/flu/video/inever_60sec.mp4"
+    
     var url:URL?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let startTime = CMTime(seconds: 5, preferredTimescale: 1)
+        let startTimeVal = NSValue.init(time: startTime)
+        
+        //Load video assets
+        let asset1Generator = AVAssetImageGenerator(
+            asset: AVAsset(url: URL(string: self.video1Url)!))
+        asset1Generator.generateCGImagesAsynchronously(forTimes: [startTimeVal]) { (requestedTime, image, actualTime, result, error) in
+            if let myImage = image {
+                print("got an image")
+                DispatchQueue.main.async {
+                    self.thumbNail1.image = UIImage(cgImage: myImage)
+                }
+            } else {
+                print(error.debugDescription)
+            }
+        }
+        
+        let asset2Generator = AVAssetImageGenerator(
+            asset: AVAsset(url: URL(string: self.video2Url)!))
+        asset2Generator.generateCGImagesAsynchronously(forTimes: [startTimeVal]) { (requestedTime, image, actualTime, result, error) in
+            if let myImage = image {
+                print("got an image")
+                DispatchQueue.main.async {
+                    self.thumbNail2.image = UIImage(cgImage: myImage)
+                }
+            } else {
+                print(error.debugDescription)
+            }
+        }
+        
+        let asset3Generator = AVAssetImageGenerator(
+            asset: AVAsset(url: URL(string: self.video3Url)!))
+        asset3Generator.generateCGImagesAsynchronously(forTimes: [startTimeVal]) { (requestedTime, image, actualTime, result, error) in
+            if let myImage = image {
+                print("got an image")
+                DispatchQueue.main.async {
+                    self.thumbNail3.image = UIImage(cgImage: myImage)
+                }
+            } else {
+                print(error.debugDescription)
+            }
+        }
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?)
     {
-        print("prepare for segue")
         let destinationVC = segue.destination as! AVPlayerViewController
         destinationVC.delegate = self
         
@@ -51,25 +92,18 @@ class VideoViewController: UIViewController, AVPlayerViewControllerDelegate  {
     }
     
     @IBAction func playVideo1(_ sender: Any) {
-        print("play video 1")
-        self.url = URL(string: "https://www.cdc.gov/flu/video/flushots_60sec.mp4")
-        //https://www.cdc.gov/flu/video/flushots_60sec.mp4
-        
+        self.url = URL(string: self.video1Url)
         performSegue(withIdentifier: "PlayVideo", sender: self)
     }
     
     @IBAction func playVideo2(_ sender: Any) {
-        print("play video 2")
-        self.url = URL(string: "https://www.cdc.gov/flu/video/who-needs-flu-vaccine-30_320px.mp4")
-        //https://www.cdc.gov/flu/video/who-needs-flu-vaccine-30_320px.mp4
-                performSegue(withIdentifier: "PlayVideo", sender: self)
+        self.url = URL(string: self.video2Url)
+        performSegue(withIdentifier: "PlayVideo", sender: self)
     }
     
     @IBAction func playVideo3(_ sender: Any) {
-        print("play video 3")
-        self.url = URL(string: "https://www.cdc.gov/flu/video/inever_60sec.mp4")
-        //https://www.cdc.gov/flu/video/inever_60sec.mp4
-                performSegue(withIdentifier: "PlayVideo", sender: self)
+        self.url = URL(string: self.video3Url)
+        performSegue(withIdentifier: "PlayVideo", sender: self)
     }
     
 }
